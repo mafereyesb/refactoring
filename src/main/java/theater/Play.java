13fedb2ce9544652004e71915affffbc4fa469da
Maria Fernanda Reyes <mariafreyes05@gmail.com>
2025-11-24 14:45:09 -0500

    Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,25 @@
 package theater;
 
+/**
+ * Represents a play in the system.
+ *
+ * @null This class does not accept null values.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
